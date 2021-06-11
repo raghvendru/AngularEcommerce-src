0262543c51118c2e1c815c9520e3bb1ff3498f11
ragavyshnavi <srivyshnavi27@gmail.com>
2021-06-11 14:03:37 +0530

    added paths

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartCompComponent } from './comp/cart-comp/cart-comp.component';
+import { CheckoutCompComponent } from './comp/checkout-comp/checkout-comp.component';
+import { LandingComponent } from './comp/landing/landing.component';
+import { LoginComponent } from './comp/login/login.component';
+import { OrderListComponent } from './comp/order-list/order-list.component';
+import { ProductDetailComponent } from './comp/product-detail/product-detail.component';
+import { ProductListComponent } from './comp/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "login", component: LoginComponent},
+  { path: "productlist", component: ProductListComponent},
+  { path: "productdetail", component: ProductDetailComponent},
+  { path: "cart", component: CartCompComponent},
+  { path: "orderlist", component: OrderListComponent},
+  { path: "landing", component: LandingComponent},
+  { path: "checkout", component: CheckoutCompComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
